7a7c09bd7b76c1b93f7d01bdfd33bd89d114b34a
GOURAVM <gouravma@cybage.com>
2022-12-05 10:26:49 +0530

    add comment

--- NFCReaderWriter/NFCReaderWriter/ViewController/WriterViewController.swift
+++ NFCReaderWriter/NFCReaderWriter/ViewController/WriterViewController.swift
@@ -52,20 +52,11 @@ class WriterViewController: UIViewController {
             }
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
+// MARK: - UITextViewDelegate
 extension WriterViewController : UITextViewDelegate {
+    
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if text == "\n" {
             textView.resignFirstResponder()
